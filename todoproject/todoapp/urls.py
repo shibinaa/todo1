28fac2fa979d2412ea72d229afdb2daa42a26330
shibina <shibinanazeem@gmail.com>
2022-12-15 08:20:42 +0530

    first commit

---
+++ todoproject/todoapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+# app_name="todoapp"
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+    path('cbvdetail/<int:pk>',views.Taskdetailview.as_view(),name='cbvdetail'),
+    path('cbvupdate/<int:pk>',views.TaskUpdateview.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>',views.TaskDelete.as_view(),name='cbvdelete'),
+]
